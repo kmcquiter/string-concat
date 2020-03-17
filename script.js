a74dc33d7b6fa3fe1d5a68ef281dc4fce9189424
kmcquiter <karlishamcquitter@gmail.com>
2020-03-16 20:43:43 -0400

    An exercise using the  String Concat method

---
+++ script.js
@@ -0,0 +1,30 @@
+
+ 
+ 
+ let greeting = "Hello ";
+ let firstName =  "Will ";
+ let lastName = "Smith! ";
+ let positiveMsg = "You are awesome! ";
+ let result = greeting.concat(firstName,lastName,positiveMsg);
+ alert(result);
+
+
+ //using prompts
+
+ let greeting = "hello";
+
+ const firstName = prompt("Enter your First Name: ");
+ console.log(firstName);
+ const lastName = prompt("Enter last last name");
+ console.log(lastName);
+ const occupation = prompt("What is your title?");
+ console.log(occupation);
+ 
+ let space = " ";
+
+ let result = greeting.concat(space, firstName , space, lastName, space , occupation);
+ alert(result);
+
+
+
+
